3e5f9f9a84e1ad977ed586f9e957cde5800c288b
Roberto Liccardo <roberto_liccardo@outlook.it>
2023-10-01 11:02:27 +0200

    fixed styling for news page

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("news", views.news, name="news"),
+    path("categories", views.categories, name="categories"),
+    path("subscribe", views.subscribe, name="subscribe"),
+    path("newsletter", views.newsletter, name="newsletter")
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,19 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "blog/index.html")
+
+
+def news(request):
+    return render(request, "blog/news.html")
+
+
+def categories(request):
+    return render(request, "blog/categories.html")
+
+
+def subscribe(request):
+    return render(request, "blog/subscribe.html")
+
+
+def newsletter(request):
+    return render(request, "blog/newsletter.html")
